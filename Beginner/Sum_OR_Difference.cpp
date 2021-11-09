7ae906eca9f842e56fa020340b1648bfadc85c77
unknown <shahariar.anu@northsouth.edu>
2021-11-09 23:50:25 +0600

    Beginner - Sum Or Difference

---
+++ Beginner/Sum_OR_Difference.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+    int a,b;
+    cin>>a>>b;
+    if(a>b)
+        cout<<a-b<<"\n";
+    else
+        cout<<a+b<<"\n";
+    return 0;
+}
