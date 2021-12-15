422690672b941e61002d2afa9d06cfa7b4c6bd70
unknown <shahariar.anu@northsouth.edu>
2021-12-15 23:53:45 +0600

    Coldplay SLOOP

---
+++ Beginner/Coldplay.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+    int t,m,s;
+    cin>>t;
+    while(t--)
+    {
+        cin>>m>>s;
+        cout<<m/s<<"\n";
+    }
+    return 0;
+}
