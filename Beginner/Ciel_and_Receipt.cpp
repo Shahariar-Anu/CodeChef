377465653c635368358140c95716986d7030f913
unknown <shahariar.anu@northsouth.edu>
2022-01-02 00:02:22 +0600

    Ciel and Receipt

---
+++ Beginner/Ciel_and_Receipt.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int a,j;
+	cin >> a;
+	while(a--){
+		long b;
+		cin >> b;
+		j=b/2048;
+		b=b%2048;
+		while(b!=0){
+			j+=b%2;
+			b/=2;
+		}
+		cout << j << endl;
+	}
+}
