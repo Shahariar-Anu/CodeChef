baba02b6bd69863a5a822115fddee3054ce0307a
unknown <shahariar.anu@northsouth.edu>
2022-01-03 23:01:49 +0600

    Is It a Vowel or Consonant (VOWELTB) git status

---
+++ Beginner/Is_it_a_VOWEL_or_CONSONANT.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+	char ch;
+	cin>>ch;
+	if(ch=='A' || ch=='E' || ch=='I' || ch=='O' || ch=='U')
+        cout<<"Vowel";
+    else
+        cout<<"Consonant";
+	return 0;
+}
